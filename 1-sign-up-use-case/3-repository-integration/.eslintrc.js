d5ddcccad1f066f6beeb94c91def03d627eae580
YTRodi <yagorodi@gmail.com>
2023-08-23 13:41:28 -0300

    1.3 Initial commit

---
+++ 1-sign-up-use-case/3-repository-integration/.eslintrc.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+	extends: ["eslint-config-codely/typescript"],
+	rules: {
+		"@typescript-eslint/unbound-method": "off",
+		"@typescript-eslint/no-misused-promises": "off",
+		"@typescript-eslint/no-unsafe-argument": "off",
+	},
+	overrides: [
+		{
+			files: ["*.ts", "*.tsx"],
+			parserOptions: {
+				project: [path.join(__dirname, "tsconfig.json")],
+			},
+		},
+	],
+};
